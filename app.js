317507df971a1f11790cc77e9cf0d6092a540308
sridhar0919 <sridhar9797@gmail.com>
2021-11-17 01:13:55 +0530

    first commit

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var roomRouter = require('./routes/room');
-var customersRouter = require('./routes/customers');
 
 var app = express();
 
@@ -24,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/room', roomRouter);
-app.use('./customers', customersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
